d454fd278f033bce0d83c7c1224536dea2e3abab
nassim <nassimbzr78@gmail.com>
2025-05-24 19:14:10 +0200

    chore(calendar): sync airbnb calendar data via ical import'

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
-const { authMiddleware } = require('../middlewares/auth');
 
 // Booking routes
-router.get('/', authMiddleware, bookingController.getAll);
-router.get('/upcoming', authMiddleware, bookingController.getUpcoming);
-router.get('/property/:propertyId', authMiddleware, bookingController.getByProperty);
-router.get('/:id', authMiddleware, bookingController.getById);
-router.post('/', authMiddleware, bookingController.create);
-router.put('/:id', authMiddleware, bookingController.update);
-router.delete('/:id', authMiddleware, bookingController.delete);
+router.get('/',  bookingController.getAll);
+router.get('/upcoming',  bookingController.getUpcoming);
+router.get('/property/:propertyId',  bookingController.getByProperty);
+router.get('/:id',  bookingController.getById);
+router.post('/',  bookingController.create);
+router.put('/:id',  bookingController.update);
+router.delete('/:id',  bookingController.delete);
 
 module.exports = router; 
